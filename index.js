f5703646bae5fe11997574383edcb223fd15390f
bouquet1 <bukuleta@gmail.com>
2023-05-27 12:14:52 -0700

    payroll function missing

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
-// READ.ME has signatures of the functions. createEmployeeRecord() takes an array of 4 elements, returns object with given keys. 
+// READ.ME has signatures of the functions. createEmployeeRecord() takes an array of 4 elements, returns object with given keys.
 //Behavior: Loads Array elements into corresponding Object properties. Additionally, initialize empty Arrays on the properties timeInEvents and timeOutEvents.
 
 const employeeArr = ["Gray", "Worm", "Security", 1];
 
 function createEmployeeRecord(employeeData) {
-    const records = {
-        firstName: employeeData[0],
-        familyName: employeeData[1],
-        title: employeeData[2],
-        payPerHour: employeeData[3],
-        timeInEvents: [],
-        timeOutEvents: [],
-    };
-    return records
+  const records = {
+    firstName: employeeData[0],
+    familyName: employeeData[1],
+    title: employeeData[2],
+    payPerHour: employeeData[3],
+    timeInEvents: [],
+    timeOutEvents: [],
+  };
+  return records;
 }
-// createEmployeeRecords() - map(). Feed rceateEmployeeRecords with an aray of array. map() will iterate over the array, perform some function, and return a new array. 
-    
- // Piazza- Consider what the functions wants you to implement as the parameter (argument) for the function, and determine which method you will need to utilize on that parameter to return what the function needs to return.
+// createEmployeeRecords() - map(). Feed ceateEmployeeRecords with an aray of array. map() will iterate over the array, perform some function, and return a new array.
 
-    
-function createEmployeeRecords(employeeArr){
-    //return twoRows.map(row => {row.map((e) => e.row[0])});; 
-     return employeeArr.map(createEmployeeRecord);
+// Piazza- Consider what the functions wants you to implement as the parameter (argument) for the function, and determine which method you will need to utilize on that parameter to return what the function needs to return.
+
+function createEmployeeRecords(employeeArr) {
+  //return twoRows.map(row => {row.map((e) => e.row[0])});;
+  return employeeArr.map(createEmployeeRecord);
 }
-/* code above; creates two records, correctly assigns the first names, creates more than two records with the test. 
-*/
-
+/* code above; creates two records, correctly assigns the first names, creates more than two records with the test.
+ */
 
 // createTimeInEvent() - takes in an employee record Object, a date stamp("YYYY-MM-DD HHMM"). Returns  the employee record a new array on the record object.
 
@@ -44,15 +42,15 @@ function createEmployeeRecords(employeeArr){
 */
 
 function createTimeInEvent(employee, dataStamp) {
-    const hour = parseInt(dataStamp.substring(11));
-    const date = dataStamp.substring(0, 10);
-    const newTimeInEvent = { 
-        type: 'TimeIn',
-        hour,
-        date,
-    };
-    employee.timeInEvents.push(newTimeInEvent);
-    return employee;
+  const hour = parseInt(dataStamp.substring(11));
+  const date = dataStamp.substring(0, 10);
+  const newTimeInEvent = {
+    type: "TimeIn",
+    hour,
+    date,
+  };
+  employee.timeInEvents.push(newTimeInEvent);
+  return employee;
 }
 
 /*
@@ -69,36 +67,49 @@ function createTimeOutEvent(employee, dateStamp) {
 */
 
 function createTimeOutEvent(employee, dataStamp) {
-    const hour = parseInt(dataStamp.substring(11));
-    const date = dataStamp.substring(0, 10);
-    const newTime0utEvent = { 
-        type: 'TimeOut',
-        hour,
-        date,
-    };
-    employee.timeOutEvents.push(newTime0utEvent);
-    return employee;
+  const hour = parseInt(dataStamp.substring(11));
+  const date = dataStamp.substring(0, 10);
+  const newTime0utEvent = {
+    type: "TimeOut",
+    hour,
+    date,
+  };
+  employee.timeOutEvents.push(newTime0utEvent);
+  return employee;
 }
 
 function hoursWorkedOnDate(employee, date) {
-    // get timeInEvent and timeOutEvent from the employee record Object
-    const timeInPunch = employee.timeInEvents.find(timeIn => timeIn.date === date)//.substring(11, 12); 
-    const timeOutPunch = employee.timeOutEvents.find(timeOut => timeOut.date === date)//.substring(11, 12);
-    //extract hour value for them. 
-    const timeInPunchHour = parseInt(timeInPunch.hour.substring(11, 12));
-    const timeOutPunchHour = parseInt(timeOutPunch.hour.substring(11, 12));
-    // add 10 to substring() to reflect base 10, number system we use
-    //calculate the numbers of hours worked. TimeOut - TimeIn
-    //return timeOutPunchHour - timeInPunchHour;
-    return timeOutPunch - timeInPunch;
+  const timeInPunch = employee.timeInEvents.find((time) => time.date === date).hour / 100;
+  const timeOutPunch = employee.timeOutEvents.find((time) => time.date === date).hour / 100;
+  return parseInt(timeOutPunch - timeInPunch);
 }
 
 function wagesEarnedOnDate(employee, date) {
-    return (hoursWorkedOnDate(date))*(employee.payPerHour);
+  const wages = hoursWorkedOnDate(employee, date) * employee.payPerHour;
+  return wages;
 }
 
+function allWagesFor(employee) {
+  const datesWorked = employee.timeInEvents.map((timeIn) => timeIn.date);
+  //iterate over datesWorked, get the total wages. reduce(total, date) an initial value 0.
+  const totalWages = datesWorked.reduce((total, date) => {
+    return total + wagesEarnedOnDate(employee, date);
+  }, 0);
+  return totalWages;
+}
+
+// function calculatePayroll(employees) {
+//   let sum = 0;
+//   for (let i = 0; i < emArray.length; i++) {
+//     sum += wagesEarnedOnDate.call(employees[i]);
+//   }
+//   return sum;
+// }
 
 
+function calculatePayroll(employees, wagesEarnedOnDate, init) {
+  return employees.reduce((employee) => wagesEarnedOnDate(employee, date), init);
+}
 
-
+calculatePayroll (employees, wagesEarnedOnDate, 0)
 
